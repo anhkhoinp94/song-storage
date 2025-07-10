b34f961caa896fb9a469b9465f1f5dc309476afc
anhkhoinp94 <anhkhoi.np94@gmail.com>
2025-07-10 16:57:34 +0700

    2025-07-10_0457 PM

--- js/filesList.js
+++ js/filesList.js
@@ -1,1 +1,1 @@
-const filesList=["musics/Blue - All Rise.mp3","musics/SOOBIN - DANCING IN THE DARK.mp3"];
+const filesList=["musics/Blue - All Rise.mp3","musics/MONO - Chăm Hoa.mp3","musics/SOOBIN - DANCING IN THE DARK.mp3"];
